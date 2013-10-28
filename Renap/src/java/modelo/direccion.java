c3da893a28b2b3b8b110ffc4080daba37282644b
Francisco Rodriguez <guatecremas_gp@yahoo.es>
2013-10-27 18:21:41 -0600

    cree el modelo direccion

---
+++ Renap/src/java/modelo/direccion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author francisco
+ */
+public class direccion {
+    protected String pais;
+    protected int departamentto;
+    protected int municipio;
+    protected String asentamiento;
+    protected int zona;
+    protected String calle;
+    protected String avenida;
+    protected int numeroCasa;
+    
+}
