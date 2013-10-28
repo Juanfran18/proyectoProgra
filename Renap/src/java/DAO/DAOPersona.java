07211f6b281e413eaf003be02b3e3c26af19bd16
Francisco Rodriguez <guatecremas_gp@yahoo.es>
2013-10-27 22:40:00 -0600

    Cree el DAO de persona Es el que se encarga de los ABC de la tabla persona

--- Renap/src/java/DAO/DAOPersona.java
+++ Renap/src/java/DAO/DAOPersona.java
@@ -21,14 +21,14 @@ public class DAOPersona {
     
     public void nuevaPersona(persona per){
         try {
-            
-        } catch (Exception e) {
             String sql = ("INSERT INTO persona (CUI,Nombre,Apellido,Sexo,EstadoCivil,EstatusNacional,FechaDeNacimiento,"
                     + "LugardeNacimiento,FechaDeDefuncion,LugarDeDefuncion,vecindad,Firma,Foto)"
                     + " VALUES ("+per.getCui()+","+per.getNombre()+","+per.getApellido()+","+per.getSexo()+","
                     +per.getEstadoCivil()+","+per.getEstatusNacional()+","+per.getFechaNacimiento()+","+per.getIdLugarNacimiento()
                     +","+per.getFechaDefuncion()+","+per.getIdLugarDefuncion()+","+per.getFirma()+","+per.getFoto()+")");
             this.con.ejecutado(sql);
+        } catch (Exception e) {
+            
             
         }
         
