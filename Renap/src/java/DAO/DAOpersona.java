b2943ef58d63ce147757ef822f36a8f6d8d8baae
Francisco Rodriguez <guatecremas_gp@yahoo.es>
2013-10-30 23:25:16 -0600

    Modifique el DAOPersona Agregando el Query y clase edit

--- Renap/src/java/DAO/DAOpersona.java
+++ Renap/src/java/DAO/DAOpersona.java
@@ -23,41 +23,39 @@ public class DAOpersona {
         this.con = BDconexion.open(BDconexion.mysql);
     }
 
-    public persona edit(String cui){
+    public persona edit(persona per) {
         this.con = BDconexion.open(BDconexion.mysql);
-        persona per = new persona();
-        String sql ="Select * from Persona where CUI="+cui;
         try {
-            ResultSet rs = this.con.query(sql);
-            while(rs.next()){
-                per.setCui(rs.getString("CUI"));
-                per.setNombre(rs.getString("Nombre"));
-                 per.setApellido(rs.getString("Apellido"));
-            }
-                
+            String sql = "UPDATE persona SET Nombre='" + per.getNombre() + "', Apellido='" + per.getApellido()
+                    + "', Sexo='" + per.getSexo() + "', EstadoCivil='" + per.getEstadoCivil() + "', EstatusNacional='" + per.getEstatusNacional()
+                    + "', FechaDeNacimiento='" + per.getFechaNacimiento() + "', LugarDeNacimiento='" + per.getIdLugarNacimiento()
+                    + "',FechaDeDefuncion='" + per.getFechaDefuncion() + "', LugarDeDefuncion='" + per.getIdLugarDefuncion()
+                    + "', Vecindad='" + per.getIdVecindad() + "' Firma='" + per.getFirma() + "', Foto='" + per.getFoto() + "'";
+            this.con.ejecutado(sql);
         } catch (Exception e) {
+            Logger.getLogger(DAOpersona.class.getName()).log(Level.SEVERE, null, e);
         }
         return per;
-        
+
     }
-    
+
     public boolean save(persona per) {
         this.con = BDconexion.open(BDconexion.mysql);
-        boolean save = false;             
+        boolean save = false;
         try {
             String sql = "INSERT INTO persona(CUI,Nombre,Apellido,Sexo,EstadoCivil,EstatusNacional,FechaDeNacimiento,"
                     + "LugarDeNacimiento,FechaDeDefuncion,LugarDeDefuncion,Vecindad,Firma,Foto)"
                     + "VALUES('" + per.getCui() + "','" + per.getNombre() + "','" + per.getApellido() + "','" + per.getSexo() + "','"
                     + per.getEstadoCivil() + "','" + per.getEstatusNacional() + "','" + per.getFechaNacimiento() + "','"
-                    + per.getIdLugarNacimiento() + "','" + per.getFechaDefuncion() + "','" + per.getIdLugarDefuncion() 
-                    + "','" + per.getIdVecindad()+"','"+per.getFirma()+"','"+per.getFoto()+ "')";
+                    + per.getIdLugarNacimiento() + "','" + per.getFechaDefuncion() + "','" + per.getIdLugarDefuncion()
+                    + "','" + per.getIdVecindad() + "','" + per.getFirma() + "','" + per.getFoto() + "')";
             this.con.ejecutado(sql);
-            
+
 
         } catch (Exception e) {
-            
+
             Logger.getLogger(DAOpersona.class.getName()).log(Level.SEVERE, null, e);
-        }finally{
+        } finally {
             this.con.cerrar();
         }
         return save;
